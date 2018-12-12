35d3f3261815c140465148321f38e90159818d6b
tian <tian@tiandeMacBook-Pro.local>
2018-12-12 22:11:21 +0800

    1.0.0.

---
+++ GRDB/GRDB.h
@@ -0,0 +1,19 @@
+//
+//  GRDB.h
+//  GRDB
+//
+//  Created by tian on 2018/12/12.
+//  Copyright © 2018 tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GRDB.
+FOUNDATION_EXPORT double GRDBVersionNumber;
+
+//! Project version string for GRDB.
+FOUNDATION_EXPORT const unsigned char GRDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRDB/PublicHeader.h>
+
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by tian on 2018/12/12.
+//  Copyright © 2018 tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
